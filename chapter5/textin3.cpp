4611c93e43805213c0ffafc0f363310539e4b32c
dga1t <dpsmnsk@gmail.com>
2021-01-01 19:02:31 +0500

    1 jan 2021

---
+++ chapter5/textin3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+  using namespace std;
+  char ch;
+  int count = 0;
+  cin.get(ch);
+
+  while (cin.fail() == false) // test for EOF
+  {
+    cout << ch;
+    ++count;
+    cin.get(ch);
+  }
+
+  cout << endl << count << " characters read\n";
+  
+  return 0;
+}
