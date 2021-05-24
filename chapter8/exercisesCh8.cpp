9a66bb9b7cbebc48903e9c847f57617cfce0f671
dga1t <dpsmnsk@gmail.com>
2021-05-24 22:26:30 +0500

    im back

---
+++ chapter8/exercisesCh8.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+
+using namespace std;
+
+void ex1();
+void ex2();
+void ex3();
+void ex4();
+void ex5();
+void ex6();
+void ex7();
+
+int main()
+{
+  ex1();
+  // ex2();
+  // ex3();
+  // ex4();
+  // ex5();
+  // ex6();
+  // ex7();
+}
+
+void ex1()
+{
+ 
+}
